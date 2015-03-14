85607b445ebecde8ea65cd20907942d4d00d3dff
Ryan Tucker <git@ryantucker.us>
2015-03-14 13:52:28 -0400

    Classify member dues on invoice import

--- invoices-invoiceable.py
+++ invoices-invoiceable.py
@@ -11,35 +11,40 @@ for row in incsv:
         continue
 
     if row['total'] == "100.00":
-        desc = "Monthly Invoice - Room"
+        desc = "Monthly Invoice - Desk"
+        acct = "Income:Member Dues:Desk"
     elif row['total'] == "50.00":
-        desc = "Monthly Invoice - Standard"
+        desc = "Monthly Invoice - Full"
+        acct = "Income:Member Dues:Full"
     elif row['total'] == "35.00":
         desc = "Monthly Invoice - Student"
+        acct = "Income:Member Dues:Student"
     elif row['total'] == "25.00":
         desc = "Monthly Invoice - Associate"
+        acct = "Income:Member Dues:Associate"
     else:
         desc = "Monthly Invoice - UNKNOWN"
+        acct = "Income:Member Dues"
 
     date = row['date'].split(' ')[0]
     duedate = row['date_due'].split(' ')[0]
 
     outcsv.writerow([
         "INV" + row['ref'],     # id
-        #row['ref'],     # id
+        #row['ref'],            # id
         date,                   # date_opened
         row['client_id'],       # owner_id
         "",                     # billingid
-        "invoiceable import",                     # notes
+        "invoiceable import",   # notes
         date,                   # date
         desc,                   # desc
-        "Material",                 # action
-        "Income:Member Dues",   # account
+        "Material",             # action
+        acct,                   # account
         "1",                    # quantity
         row['total'],           # price
         "", # disc_type
         "", # disc_how
-        "",                    # discount
+        "",                     # discount
         "", # taxable
         "",                     # taxincluded
         "",                     # tax_table
