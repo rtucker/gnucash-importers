160a0eeda10dc44ad893430be0cf49e77b2b3f41
Ryan Tucker <git@ryantucker.us>
2016-03-08 22:40:14 -0500

    update invoicable to support txn fee split

--- invoices-invoiceable.py
+++ invoices-invoiceable.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
+from decimal import Decimal
 import csv
 import sys
 
 POST_ACCOUNT = "Assets:Accounts Receivable"
+MAIN_ACCOUNT = "Income:Member Dues"
+
+AMOUNTS = {
+    Decimal("100.00"): 'Desk',
+    Decimal("50.00"):  'Full',
+    Decimal("35.00"):  'Student',
+    Decimal("25.00"):  'Associate',
+}
+
+TRANSACTION_FEE = Decimal("0.03")   # multiplier
+
+TXN_FEE_AMOUNTS = [f * TRANSACTION_FEE for f in AMOUNTS.keys()]
 
 incsv = csv.DictReader(sys.stdin)
 outcsv = csv.writer(sys.stdout)
@@ -12,24 +25,52 @@ for row in incsv:
     if int(row['recurring']) == 1:
         continue
 
-    if row['total'] == "100.00":
-        desc = "Monthly Invoice - Desk"
-        acct = "Income:Member Dues:Desk"
-    elif row['total'] == "50.00":
-        desc = "Monthly Invoice - Full"
-        acct = "Income:Member Dues:Full"
-    elif row['total'] == "35.00":
-        desc = "Monthly Invoice - Student"
-        acct = "Income:Member Dues:Student"
-    elif row['total'] == "25.00":
-        desc = "Monthly Invoice - Associate"
-        acct = "Income:Member Dues:Associate"
+    total = Decimal(row['total'])
+    txn_fee = Decimal("0.00")
+
+    for k in AMOUNTS.keys():
+        for f in TXN_FEE_AMOUNTS:
+            if total == (f + k):
+                unknown = False
+                txn_fee = f
+                total -= f
+
+    if total in AMOUNTS:
+        desc = "Monthly Dues - " + AMOUNTS[total]
+        acct = ":".join([MAIN_ACCOUNT, AMOUNTS[total]])
     else:
         desc = "Monthly Invoice - UNKNOWN"
         acct = "Income:Member Dues"
 
     date = row['date'].split(' ')[0]
     duedate = row['date_due'].split(' ')[0]
+
+    if txn_fee > Decimal("0.00"):
+        outcsv.writerow([
+            "INV" + row['ref'],     # id
+            date,                   # date_opened
+            row['client_id'],       # owner_id
+            "",                     # billingid
+            "invoiceable import",   # notes
+            date,                   # date
+            "Payment Processing Surcharge (%g%%)" % (
+                TRANSACTION_FEE*Decimal("100.0")),
+            "Material",             # action
+            MAIN_ACCOUNT + ":Payment Processing Surcharge",
+            total,                  # quantity
+            TRANSACTION_FEE,        # price
+            "%",                    # disc_type
+            "",                     # disc_how
+            "0",                    # discount
+            "",                     # taxable
+            "",                     # taxincluded
+            "",                     # tax_table
+            date,                   # date_posted
+            duedate,                # due_date
+            POST_ACCOUNT,           # account_posted
+            "",                     # memo_posted
+            "yes",                  # accu_splits
+        ])
 
     outcsv.writerow([
         "INV" + row['ref'],     # id
@@ -42,7 +83,7 @@ for row in incsv:
         "Material",             # action
         acct,                   # account
         "1",                    # quantity
-        row['total'],           # price
+        total,                  # price
         "%",                    # disc_type
         "",                     # disc_how
         "0",                    # discount
